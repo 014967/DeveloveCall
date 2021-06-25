5cb9f4ab88c0e014789935c9a1dc36a0728c3022
boris <boris0815@naver.com>
2021-06-25 15:36:10 +0900

    feat :: amplify

---
+++ app/src/main/java/com/example/developCall/LaunchActivity.java
@@ -0,0 +1,37 @@
+package com.example.developCall;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.amplifyframework.auth.AuthUser;
+import com.amplifyframework.core.Amplify;
+
+public class LaunchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+
+
+        AuthUser currentUser = Amplify.Auth.getCurrentUser();
+        Intent intent;
+        if(currentUser == null )
+        {
+            //Go to the Login Page
+
+             intent = new Intent(getApplicationContext(), LoginActivity.class);
+
+        }
+        else
+        {
+            //Go to the Main Page
+
+             intent = new Intent(getApplicationContext(), MainActivity.class);
+        }
+        startActivity(intent);
+        finish();
+    }
+}
--- app/src/main/java/com/example/developCall/LoginActivity.java
+++ app/src/main/java/com/example/developCall/LoginActivity.java
@@ -4,12 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.amplifyframework.auth.AuthUser;
+import com.amplifyframework.core.Amplify;
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+
 
 
     }
