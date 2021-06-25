70d5e2634f3ece4e0b98abbfbeb7a014c878ba77
boris <boris0815@naver.com>
2021-06-25 22:06:35 +0900

    feat : login

---
+++ app/src/main/java/com/example/developCall/EmailConfirmationActivity.java
@@ -0,0 +1,105 @@
+package com.example.developCall;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.amplifyframework.auth.AuthException;
+import com.amplifyframework.auth.result.AuthSignInResult;
+import com.amplifyframework.auth.result.AuthSignUpResult;
+import com.amplifyframework.core.Amplify;
+import com.amplifyframework.core.model.Model;
+import com.amplifyframework.datastore.DataStoreException;
+import com.amplifyframework.datastore.DataStoreItemChange;
+import com.amplifyframework.datastore.generated.model.User;
+import com.amplifyframework.datastore.generated.model.User.Builder;
+
+public class EmailConfirmationActivity extends AppCompatActivity {
+
+
+    EditText Confirmation_Code;
+    Button btn_Confirm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_email_confirmation);
+
+
+        Confirmation_Code = (EditText)findViewById(R.id.Confirmation_Code);
+        btn_Confirm = (Button)findViewById(R.id.btn_Confirm);
+
+        Amplify.Auth.confirmSignUp(
+                getEmail(),
+                Confirmation_Code.getText().toString(),
+                this::onSuccess,
+                this::onError
+        );
+
+
+    }
+    private void onError(AuthException e )
+    {
+
+        runOnUiThread(()->
+        {
+            Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_LONG).show();
+        });
+
+    }
+
+    private void onSuccess(AuthSignUpResult authSignUpResult)
+    {
+        reLogin();
+    }
+
+    private void reLogin()
+    {
+      String username = getEmail();
+      String password = getPassWord();
+
+      Amplify.Auth.signIn(
+              username,
+              password,
+              this::onLoginSuccess,
+              this::onError
+      );
+    }
+
+    private void onLoginSuccess(AuthSignInResult authSignInResult) {
+        String userId = Amplify.Auth.getCurrentUser().getUserId();
+
+        String name= getName();
+
+        Amplify.DataStore.save(
+                User.builder().name(name).id(userId).build(),
+                this::onSavedSuccess,
+                this::onError
+        );
+    }
+
+    private void onError(DataStoreException e) {
+    }
+
+    private <T extends Model> void onSavedSuccess(DataStoreItemChange<T> tDataStoreItemChange)
+    {
+        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+        startActivity(intent);
+    }
+    private String getEmail(){
+        return getIntent().getStringExtra("email");
+    }
+    private String getPassWord(){
+        return getIntent().getStringExtra("password");
+    }
+    private String getName(){
+        return getIntent().getStringExtra("name");
+    }
+
+
+}
--- app/src/main/java/com/example/developCall/LoginActivity.java
+++ app/src/main/java/com/example/developCall/LoginActivity.java
@@ -2,17 +2,84 @@ package com.example.developCall;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.amplifyframework.auth.AuthException;
 import com.amplifyframework.auth.AuthUser;
+import com.amplifyframework.auth.result.AuthSignInResult;
 import com.amplifyframework.core.Amplify;
 
 public class LoginActivity extends AppCompatActivity {
 
+
+
+    Button btn_login;
+    EditText user_Email;
+    EditText user_Password;
+    TextView txt_Join;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.login);
+
+
+        btn_login = (Button)findViewById(R.id.btn_login);
+        txt_Join = (TextView) findViewById(R.id.txt_join);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                user_Email = (EditText)findViewById(R.id.txt_id);
+                user_Password = (EditText)findViewById(R.id.PassWord);
+
+                Amplify.Auth.signIn(
+                        user_Email.getText().toString(),
+                        user_Password.getText().toString(),
+                        this::onLoginSuccess,
+                        this::onLoginError
+                );
+
+            }
+
+            private void onLoginSuccess(AuthSignInResult authSignInResult) {
+
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+
+            private void onLoginError(AuthException e )
+            {
+                runOnUiThread(()->
+                {
+
+                    Toast.makeText(getApplicationContext(),e.getMessage(), Toast.LENGTH_LONG).show();
+                });
+
+
+            }
+
+
+        });
+
+        txt_Join.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                    Intent intent = new Intent(getApplicationContext(),JoinActivity.class);
+                    startActivity(intent);
+                }
+
+        });
 
 
 
